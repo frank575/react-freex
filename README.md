4295258545f68e6b258412f3dd57ed6e605a4558
§d«T½n <qaz199671@gmail.com>
2020-03-28 13:27:25 +0800

    0.3.3 -- 代碼優化，移除 class On

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # react-freex
 
 > 最輕量簡單的 React 狀態管理
+> Lightest and simplest React state management
 
 [![Edit react-freex on CodeSandbox](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/react-freex-h5dk6)
 
@@ -21,10 +22,11 @@
 
 #### 更新日誌：
 
-v0.3.2 -- 更新 README.md  
+v0.3.3 -- 代碼優化
+v0.3.2 -- 更新 README.md
 v0.3.1 -- 更新 README.md 及 getters 的資料存放位置  
-v0.3.0 -- 新增 $set 方法，及性能小優化，支持自動 dispatch  
+v0.3.0 -- 新增 \$set 方法，及性能小優化，支持自動 dispatch  
 v0.2.1 -- 更新 README.md  
 v0.2.0 -- 更新時導致全組件渲染的效能浪費修正，調用方式調整  
 v0.1.0 -- 性能優化及支持 getters，調用方式優化  
-v0.0.0  
+v0.0.0
--- react-freex.js
+++ react-freex.js
@@ -1,109 +1,97 @@
-import React, { createContext, useReducer, useEffect } from "react";
-/** v0.3.1 */
-const fx = {};
-const Providers = [];
+import React, { createContext, useReducer, useEffect } from 'react'
+/** v0.3.3 */
+const fx = {}
+const Providers = []
 const FreexProvider = props => {
-  return Providers.reduceRight((BP, AP) => <AP>{BP}</AP>, props.children);
-};
-class On {
-  constructor(fn, store) {
-    this._fn = fn;
-    this.store = store;
-    this._dispatch = null;
-  }
-  set dispatch(dispatch) {
-    return (this._dispatch = dispatch);
-  }
-  call(...args) {
-    const _dispatch = this._dispatch;
-    return _dispatch._fn(this._fn.apply(_dispatch.store, args));
-  }
+  return Providers.reduceRight((BP, AP) => <AP>{BP}</AP>, props.children)
 }
 const createProviders = store => {
-  const _store = store;
-  let providerValue = {};
-  let isSave = false;
-  const saves = {};
-  const fxStorage = localStorage.fx;
-  const jsonFxStorage = fxStorage ? JSON.parse(fxStorage) : {};
+  const _store = store
+  let providerValue = {}
+  let isSave = false
+  const saves = {}
+  const fxStorage = localStorage.fx
+  const jsonFxStorage = fxStorage ? JSON.parse(fxStorage) : {}
   for (const key in _store) {
-    const store = _store[key];
-    const state = {};
-    const storage = jsonFxStorage[key] || {};
-    const _dispatch = new On(null, state);
+    const store = _store[key]
+    const state = {}
+    const storage = jsonFxStorage[key] || {}
+    const _dispatch = {
+      state,
+      fn: null,
+    }
     for (const skey in store) {
-      const cur = store[skey];
-      if (typeof cur === "function") {
+      const cur = store[skey]
+      if (typeof cur === 'function') {
         if (/^_{2}/.test(skey)) {
-          const rpSkey = skey.replace(/^_{2}/, "");
-          state[rpSkey] = cur.call(state);
+          const rpSkey = skey.replace(/^_{2}/, '')
+          state[rpSkey] = cur.call(state)
           if (state.__getters) {
-            state.__getters[rpSkey] = cur;
+            state.__getters[rpSkey] = cur
           } else {
-            state.__getters = {};
-            state.__getters[rpSkey] = cur;
+            state.__getters = {}
+            state.__getters[rpSkey] = cur
           }
         } else {
-          const on = new On(cur, null);
-          on.dispatch = _dispatch;
-          state[skey] = on.call.bind(on);
+          state[skey] = (...args) =>
+            _dispatch.fn(cur.apply(_dispatch.state, args))
         }
       } else {
         if (/^\$/.test(skey)) {
-          const afterKey = skey.substr(1, skey.length - 1);
-          const save = saves[key];
-          const stor = storage[afterKey];
-          !isSave && (isSave = true);
-          if (save) save = { ...save, [afterKey]: cur };
-          else saves[key] = { [afterKey]: cur };
-          if (stor !== undefined) state[afterKey] = stor;
-          else state[afterKey] = cur;
+          const afterKey = skey.substr(1, skey.length - 1)
+          const save = saves[key]
+          const stor = storage[afterKey]
+          !isSave && (isSave = true)
+          if (save) save = { ...save, [afterKey]: cur }
+          else saves[key] = { [afterKey]: cur }
+          if (stor !== undefined) state[afterKey] = stor
+          else state[afterKey] = cur
         } else {
-          state[skey] = cur;
+          state[skey] = cur
         }
       }
     }
-    fx[key] = createContext();
-    Object.defineProperty(state, "$fx", {
+    fx[key] = createContext()
+    Object.defineProperty(state, '$fx', {
       get() {
-        return providerValue;
+        return providerValue
       },
-      enumerable: true
-    });
+      enumerable: true,
+    })
     Providers.push(props => {
-      const Fx = fx[key];
-      let pVal = {};
+      const Fx = fx[key]
+      let pVal = {}
       const reFunc = (_, result) => {
-        const getters = result.__getters;
-        _dispatch.store = result;
+        const getters = result.__getters
+        _dispatch.state = result
         if (isSave) {
           for (const svkey in saves[key]) {
-            saves[key][svkey] = result[svkey];
+            saves[key][svkey] = result[svkey]
           }
-          localStorage.setItem("fx", JSON.stringify(saves));
+          localStorage.setItem('fx', JSON.stringify(saves))
         }
         if (getters) {
           for (const key in getters) {
-            const rpKey = key.replace(/^_{2}/, "");
-            result[rpKey] = getters[rpKey].call(result);
+            const rpKey = key.replace(/^_{2}/, '')
+            result[rpKey] = getters[rpKey].call(result)
           }
         }
-        return result;
-      };
-      const [reducer, dispatch] = useReducer(reFunc, state);
-      providerValue[key] = reducer;
-      pVal = providerValue[key];
+        return result
+      }
+      const [reducer, dispatch] = useReducer(reFunc, state)
+      providerValue[key] = reducer
+      pVal = providerValue[key]
       useEffect(() => {
         pVal.$set = (key, val) =>
           dispatch({
             ...pVal,
-            [key]: val
-          });
-        _dispatch._fn = dispatch;
-      }, []);
-      return <Fx.Provider value={pVal}>{props.children}</Fx.Provider>;
-    });
+            [key]: val,
+          })
+        _dispatch.fn = dispatch
+      }, [])
+      return <Fx.Provider value={pVal}>{props.children}</Fx.Provider>
+    })
   }
-};
-export default createProviders;
-export { fx, FreexProvider };
+}
+export default createProviders
+export { fx, FreexProvider }
